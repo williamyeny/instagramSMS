9cf0862ba9061b10c7e52aa42ed6141c4bedbeda
williamyeny <williamyeny@gmail.com>
2016-04-16 21:06:38 -0400

    keks

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var currentTime;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/views");
 app.use(express.static(path.join(__dirname, 'public')));
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 var csrf;
 var session;
@@ -28,7 +33,7 @@ app.post('/setup', function(req, res) {
 });
 
 app.post('/getsms', function(req, res) {
-  for (key in req) {
+  for (key in req.body) {
     console.log('key: ' + key);
   }
   var twiml = new twilio.TwimlResponse();
